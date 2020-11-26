928d476af56fcb1ebab346cebb12f66079529542
Tim Dinsdale <tim@openfin.co>
2020-11-26 15:12:45 +0000

    fdc3

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'
 import {Hello} from './components/Hello/Hello';
+import * as fdc3 from 'openfin-fdc3';
+
+fdc3.getSystemChannels().then((chs)=>{
+    chs[0].join().then(()=>{
+        fdc3.broadcast({
+            "type" : "fdc3.instrument",
+            "name" : "Apple",
+            "id" : 
+            {  
+                "ticker" : "aapl",
+                "ISIN" : "US0378331005",
+                "CUSIP" : "037833100",
+                "FIGI" : "BBG000B9XRY4",
+            }
+        });
+    })
+})
+
 
 ReactDOM.render(<Hello/>, document.getElementById('app'));
